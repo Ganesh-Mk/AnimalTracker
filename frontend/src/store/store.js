03be6c21b944b0c73493fba7e22ebcfa16f9c9f6
Ganesh <ganeshmk247@gmail.com>
2024-06-03 23:49:15 +0530

    map component initial

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import userReducer from './userSlice'
 import animalReducer from './animalSlice'
+import mapReducer from './mapSlice'
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     animal: animalReducer,
+    map: mapReducer,
   },
 })
 
